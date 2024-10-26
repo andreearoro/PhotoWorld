10b0f1f0661c095181c6110d63790a51ba945497
andreearoro <andreearoman26@yahoo.com>
2024-10-26 11:00:51 +0000

    feature: display on home page

--- api/alimente.js
+++ api/alimente.js
@@ -1,5 +1,4 @@
-const URL = 'https://670fe5c2a85f4164ef2c625c.mockapi.io/alimente';
-
+const URL = "https://670fe5c2a85f4164ef2c625c.mockapi.io/PhotoWorld"
 export async function getAllProducts() {
     const response = await fetch(URL);
     const products = await response.json();
