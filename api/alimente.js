18af7f1deaa8257522789856fb68297c54f52605
andreearoro <andreearoman26@yahoo.com>
2024-10-26 09:46:07 +0000

    add: js, alimente

---
+++ api/alimente.js
@@ -0,0 +1,8 @@
+const URL = 'https://670fe5c2a85f4164ef2c625c.mockapi.io/alimente';
+
+export async function getAllProducts() {
+    const response = await fetch(URL);
+    const products = await response.json();
+
+    return products;
+}
